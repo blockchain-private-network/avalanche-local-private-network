f602b7795e236e0d846b2b174988136a28a95df3
Shiming <codingmylife@gmail.com>
2022-04-06 18:39:24 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# Avalanche local private network
+
+Scripts to build avalanche local private network.
+
+## Get started
+
+- start network: `./start.sh`
+- stop network: `./stop.sh`
+- clean data and logs: `./clean.sh`
+
+## Init fund
+
+50000000 AVAX per-fund in
+
+C chain address: 0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC
+
+C chain private key: 0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027
+
+## Default C chain config
+
+https://docs.avax.network/build/references/avalanchego-config-flags/#c-chain-configs
+
+```json
+{
+  "snowman-api-enabled": false,
+  "coreth-admin-api-enabled": false,
+  "coreth-admin-api-dir": "",
+  "eth-apis": [
+    "public-eth",
+    "public-eth-filter",
+    "net",
+    "web3",
+    "internal-public-eth",
+    "internal-public-blockchain",
+    "internal-public-transaction-pool"
+  ],
+  "continuous-profiler-dir": "",
+  "continuous-profiler-frequency": 900000000000,
+  "continuous-profiler-max-files": 5,
+  "rpc-gas-cap": 50000000,
+  "rpc-tx-fee-cap": 100,
+  "preimages-enabled": false,
+  "snapshot-async": true,
+  "snapshot-verification-enabled": false,
+  "pruning-enabled": true,
+  "allow-missing-tries": false,
+  "populate-missing-tries-parallelism": 1024,
+  "metrics-enabled": true,
+  "metrics-expensive-enabled": false,
+  "local-txs-enabled": false,
+  "api-max-duration": 0, // Default to no maximum
+  "ws-cpu-refill-rate": 0,
+  "ws-cpu-max-stored": 0,
+  "api-max-blocks-per-request": 0, // Default to no maximum
+  "allow-unfinalized-queries": false,
+  "allow-unprotected-txs": false,
+  "keystore-directory": "",
+  "keystore-external-signer": "",
+  "keystore-insecure-unlock-allowed": false,
+  "remote-tx-gossip-only-enabled": false,
+  "tx-regossip-frequency": 60000000000,
+  "tx-regossip-max-size": 15,
+  "log-level": "info",
+  "offline-pruning-enabled": false,
+  "offline-pruning-bloom-filter-size": 512, // MB
+  "offline-pruning-data-directory": "",
+  "max-outbound-active-requests": 8
+}
+```
+
+## C chain Rpc & websockts
+
+Node0:
+
+- rpc: http://127.0.0.1:9652/ext/bc/C/rpc
+- websocket: ws://127.0.0.1:9652/ext/bc/C/ws
+
+Node1:
+
+- rpc: http://127.0.0.1:9654/ext/bc/C/rpc
+- websocket: ws://127.0.0.1:9654/ext/bc/C/ws
+
+Node2:
+
+- rpc: http://127.0.0.1:9656/ext/bc/C/rpc
+- websocket: ws://127.0.0.1:9656/ext/bc/C/ws
+
+## Api
+
+doc: https://docs.avax.network/build/tools/postman-avalanche-collection
+
+## Notes
+
+The block number will keeps 0 in local network if you don't send transation.
+
+## Reference
+
+create a local test network: https://docs.avax.network/build/tutorials/platform/create-a-local-test-network/#manually
+https://avatools.io/
+
+how to convert x-chain private key to c-chain private key:
+
+- https://github.com/ava-labs/avalanchego/issues/1043
+
+- https://github.com/ava-labs/avalanche-docs/issues/661
+
+integrate with metamask: https://docs.avax.network/build/tutorials/platform/fund-a-local-test-network/#integrate-with-metamask
---
+++ start.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd `dirname $0`
+ROOTDIR=${PWD}
+if [[ ! -e avalanchego ]]
+then
+    git clone https://github.com/ava-labs/avalanchego.git
+fi
+
+cd avalanchego
+rm -rf build
+if [[ $# -gt 0 ]]
+then
+    git pull origin master
+    git fetch --tags
+    git checkout tags/$1 
+    bash scripts/build.sh
+else
+    git pull origin master
+    bash scripts/build.sh
+fi
+
+mkdir -p ~/.avalanchego/chains/C
+
+cat > ~/.avalanchego/chains/C/config.json <<EOF
+{
+  "snowman-api-enabled": false,
+  "coreth-admin-api-enabled": false,
+  "coreth-performance-api-enabled": false,
+  "net-api-enabled": true,
+  "rpc-gas-cap": 2500000000,
+  "rpc-tx-fee-cap": 100,
+  "eth-api-enabled": true,
+  "personal-api-enabled": false,
+  "tx-pool-api-enabled": true,
+  "debug-api-enabled": true,
+  "web3-api-enabled": true,
+  "local-txs-enabled": true,
+  "pruning-enabled": false,
+  "api-max-duration": 0,
+  "api-max-blocks-per-request": 0,
+  "allow-unfinalized-queries": true,
+  "log-level": "info",
+  "eth-apis": [
+    "public-eth",
+    "public-eth-filter",
+    "net",
+    "web3",
+    "internal-public-eth",
+    "internal-public-blockchain",
+    "internal-public-transaction-pool"
+  ]
+}
+EOF
+
+(./build/avalanchego --public-ip=127.0.0.1 --http-port=9650 --staking-port=9651 --db-dir=${ROOTDIR}/db/node0 --network-id=local --staking-tls-cert-file=$(pwd)/staking/local/staker1.crt --staking-tls-key-file=$(pwd)/staking/local/staker1.key >> ${ROOTDIR}/node0.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --http-port=9652 --staking-port=9653 --db-dir=${ROOTDIR}/db/node1 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker2.crt --staking-tls-key-file=$(pwd)/staking/local/staker2.key >> ${ROOTDIR}/node1.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --http-port=9654 --staking-port=9655 --db-dir=${ROOTDIR}/db/node2 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker3.crt --staking-tls-key-file=$(pwd)/staking/local/staker3.key >> ${ROOTDIR}/node2.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --http-port=9656 --staking-port=9657 --db-dir=${ROOTDIR}/db/node3 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker4.crt --staking-tls-key-file=$(pwd)/staking/local/staker4.key >> ${ROOTDIR}/node3.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --http-port=9658 --staking-port=9659 --db-dir=${ROOTDIR}/db/node4 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker5.crt --staking-tls-key-file=$(pwd)/staking/local/staker5.key >> ${ROOTDIR}/node4.log 2>&1)&
+echo "node started"
+
+echo "wait 20 seconds for bootstraps..."
+sleep 10
+
+echo "get nodeId: "
+curl -X POST --data '{
+    "jsonrpc": "2.0",
+    "method": "info.getNodeID",
+    "params":{},
+    "id": 1
+}' -H 'content-type:application/json;' 127.0.0.1:9652/ext/info
+
+echo "connected peers: "
+curl -X POST --data '{
+    "jsonrpc":"2.0",
+    "id"     :1,
+    "method" :"info.peers"
+}' -H 'content-type:application/json;' 127.0.0.1:9652/ext/info
+
+# echo "create user: "
+# curl -X POST --data '{
+#     "jsonrpc":"2.0",
+#     "id"     :1,
+#     "method" :"keystore.createUser",
+#     "params" :{
+#         "username":"user",
+#         "password":"PfC77^%^2F"
+#     }
+# }' -H 'content-type:application/json;' 127.0.0.1:9652/ext/keystore
+
+# echo "import key: "
+# curl -X POST --data '{  
+#     "jsonrpc":"2.0",    
+#     "id"     :1,    
+#     "method" :"avax.importKey", 
+#     "params" :{ 
+#         "username" :"user",   
+#         "password":"PfC77^%^2F",    
+#         "privateKey":"PrivateKey-ewoqjP7PxY4yr3iLTpLisriqt94hdyDFNgchSxGGztUrTXtNN"    
+#     }   
+# }' -H 'content-type:application/json;' 127.0.0.1:9652/ext/bc/C/avax
+
+echo "pre-fund c chain address 0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC private key 0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"
+echo "get pre-fund balance: "
+curl --location --request POST 'http://127.0.0.1:9652/ext/bc/C/rpc' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "jsonrpc": "2.0",
+    "method": "eth_getBalance",
+    "params": [
+        "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC",
+        "latest"
+    ],
+    "id": 1
+}'
+
+# echo "get current validators:"
+# curl --location --request POST 'http://127.0.0.1:9652/ext/bc/P' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "jsonrpc": "2.0",
+#     "method": "platform.getCurrentValidators",
+#     "params": {
+#         "subnetID":null,
+#         "nodeIDs":[]
+#     },
+#     "id": 1
+# }'
