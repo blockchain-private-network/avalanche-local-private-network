71f1d0183d0dab31cd2bb112701204e4d5f462ad
Shiming <codingmylife@gmail.com>
2022-06-01 18:19:04 +0800

    refactor: ipc enable

--- start.sh
+++ start.sh
@@ -27,12 +27,12 @@ fi
 
 for ((i=1;i<=$N;i+=1))
 do
+    BASE_DIR=${ROOTDIR}/db/node$i
     NODE_CONFIG_DIR=${ROOTDIR}/db/node$i/.avalanchego
     mkdir -p ${NODE_CONFIG_DIR}/chains/C && mkdir -p ${NODE_CONFIG_DIR}/configs
     echo '{' > ${NODE_CONFIG_DIR}/configs/node.json
     echo '    "log-level": "info",' >> ${NODE_CONFIG_DIR}/configs/node.json
-    echo '    "api-ipcs-enabled": true,' >> ${NODE_CONFIG_DIR}/configs/node.json
-    echo "    \"ipcs-path\": \"${NODE_CONFIG_DIR}\"" >> ${NODE_CONFIG_DIR}/configs/node.json
+    echo "    \"ipcs-path\": \"${BASE_DIR}\"" >> ${NODE_CONFIG_DIR}/configs/node.json
     echo '}' >> ${NODE_CONFIG_DIR}/configs/node.json
 
     cat > ${NODE_CONFIG_DIR}/chains/C/config.json <<EOF
@@ -69,10 +69,10 @@ EOF
 done
 
 (./build/avalanchego --public-ip=127.0.0.1 --http-port=9650 --staking-port=9651 --db-dir=${ROOTDIR}/db/node0 --network-id=local --staking-tls-cert-file=$(pwd)/staking/local/staker1.crt --staking-tls-key-file=$(pwd)/staking/local/staker1.key >> ${ROOTDIR}/node0.log 2>&1)&
-(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node1/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node1/.avalanchego/chains --http-port=9652 --staking-port=9653 --db-dir=${ROOTDIR}/db/node1 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker2.crt --staking-tls-key-file=$(pwd)/staking/local/staker2.key >> ${ROOTDIR}/node1.log 2>&1)&
-(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node2/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node2/.avalanchego/chains --http-port=9654 --staking-port=9655 --db-dir=${ROOTDIR}/db/node2 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker3.crt --staking-tls-key-file=$(pwd)/staking/local/staker3.key >> ${ROOTDIR}/node2.log 2>&1)&
-(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node3/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node3/.avalanchego/chains --http-port=9656 --staking-port=9657 --db-dir=${ROOTDIR}/db/node3 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker4.crt --staking-tls-key-file=$(pwd)/staking/local/staker4.key >> ${ROOTDIR}/node3.log 2>&1)&
-(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node4/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node4/.avalanchego/chains --http-port=9658 --staking-port=9659 --db-dir=${ROOTDIR}/db/node4 --network-id=local --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker5.crt --staking-tls-key-file=$(pwd)/staking/local/staker5.key >> ${ROOTDIR}/node4.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node1/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node1/.avalanchego/chains --http-port=9652 --staking-port=9653 --db-dir=${ROOTDIR}/db/node1 --network-id=local api-ipcs-enabled=true --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker2.crt --staking-tls-key-file=$(pwd)/staking/local/staker2.key >> ${ROOTDIR}/node1.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node2/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node2/.avalanchego/chains --http-port=9654 --staking-port=9655 --db-dir=${ROOTDIR}/db/node2 --network-id=local api-ipcs-enabled=true --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker3.crt --staking-tls-key-file=$(pwd)/staking/local/staker3.key >> ${ROOTDIR}/node2.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node3/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node3/.avalanchego/chains --http-port=9656 --staking-port=9657 --db-dir=${ROOTDIR}/db/node3 --network-id=local api-ipcs-enabled=true --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker4.crt --staking-tls-key-file=$(pwd)/staking/local/staker4.key >> ${ROOTDIR}/node3.log 2>&1)&
+(./build/avalanchego --public-ip=127.0.0.1 --config-file=${ROOTDIR}/db/node4/.avalanchego/configs/node.json --chain-config-dir=${ROOTDIR}/db/node4/.avalanchego/chains --http-port=9658 --staking-port=9659 --db-dir=${ROOTDIR}/db/node4 --network-id=local api-ipcs-enabled=true --bootstrap-ips=127.0.0.1:9651 --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --staking-tls-cert-file=$(pwd)/staking/local/staker5.crt --staking-tls-key-file=$(pwd)/staking/local/staker5.key >> ${ROOTDIR}/node4.log 2>&1)&
 echo "node started"
 
 echo "wait 20 seconds for bootstraps..."
@@ -92,29 +92,6 @@ curl -X POST --data '{
     "id"     :1,
     "method" :"info.peers"
 }' -H 'content-type:application/json;' 127.0.0.1:9652/ext/info
-
-# echo "create user: "
-# curl -X POST --data '{
-#     "jsonrpc":"2.0",
-#     "id"     :1,
-#     "method" :"keystore.createUser",
-#     "params" :{
-#         "username":"user",
-#         "password":"PfC77^%^2F"
-#     }
-# }' -H 'content-type:application/json;' 127.0.0.1:9652/ext/keystore
-
-# echo "import key: "
-# curl -X POST --data '{  
-#     "jsonrpc":"2.0",    
-#     "id"     :1,    
-#     "method" :"avax.importKey", 
-#     "params" :{ 
-#         "username" :"user",   
-#         "password":"PfC77^%^2F",    
-#         "privateKey":"PrivateKey-ewoqjP7PxY4yr3iLTpLisriqt94hdyDFNgchSxGGztUrTXtNN"    
-#     }   
-# }' -H 'content-type:application/json;' 127.0.0.1:9652/ext/bc/C/avax
 
 echo "pre-fund c chain address 0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC private key 0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"
 echo "get pre-fund balance: "
